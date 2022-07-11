f577a70eef4db66ddbf78268ef9e936c99f4eb07
sudip-gituser <109061395+sudip-gituser@users.noreply.github.com>
2022-07-11 18:16:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 streamlit.title('Writing my first Streamlit code in GitHub')
 streamlit.title('I find this training very interesting and am really enjoying this')
+
+streamlit.header('Here is the breakfast menu that I had today')
+streamlit.text('Omega 3 & bueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
